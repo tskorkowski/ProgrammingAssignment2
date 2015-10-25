b8c6e752091b3ebf18a505e18a07114272e6cfd6
tskorkowski <tskorkowski@users.noreply.github.com>
2015-10-26 00:24:35 +0100

    finished assigmnet

--- cachematrix.R
+++ cachematrix.R
@@ -2,14 +2,35 @@
 ## functions do
 
 ## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+## This function creates a list of functions to get and set a matrix, and get and set an inverse of this matrix
+makeCacheMatrix <- function(x = matrix())
+{
+  inverse <- NULL
+  set <- function(y)
+  {
+    x <<- y
+    inverse <<- NULL
+  }
+  get <- function() x
+  setInverse <- function(inv) inverse <<- inv
+  getInverse <- function() inverse
+  list(set = set, get = get , setInverse = setInverse, getInverse = getInverse)
 }
 
 
 ## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+## The following fuction either calculates an inverse of a ginven matrix or reads it from memory if it has
+## already been chached or has changed
+cacheSolve <- function(x, ...)
+{
+  inverse <- x$getInverse()
+  if(!is.null(inverse) )
+  {
+    message("getting chached inversed matrix")
+    return(inverse)
+  }
+  inverse <- solve(x$get())
+  x$setInverse(inverse)
+  inverse
+  ## Return a matrix that is the inverse of 'x'
 }
