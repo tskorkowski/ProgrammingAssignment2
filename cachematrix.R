83d68678014e15e8884d66399f64b9d017866e7a
tskorkowski <tskorkowski@users.noreply.github.com>
2015-11-22 17:36:06 +0100

    Assignment2
    The solution to the assignment 2 of R propgramming course

--- cachematrix.R
+++ cachematrix.R
@@ -1,8 +1,11 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The aim of the following two functions is to cache the results of matrix invertion.
+## To use them efficently one needs forst to create special "matrix" with makeCacheMatrix
+## function and next operate on it by using cacheSolce function.
 
 ## Write a short comment describing this function
-## This function creates a list of functions to get and set a matrix, and get and set an inverse of this matrix
+## This function creates a special "matrix" that in fact is 
+## a list of functions to get and set a matrix, and get and set an inverse of this matrix.
+
 makeCacheMatrix <- function(x = matrix())
 {
   inverse <- NULL
@@ -19,8 +22,9 @@ makeCacheMatrix <- function(x = matrix())
 
 
 ## Write a short comment describing this function
+
 ## The following fuction either calculates an inverse of a ginven matrix or reads it from memory if it has
-## already been chached or has changed
+## already been chached
 cacheSolve <- function(x, ...)
 {
   inverse <- x$getInverse()
@@ -31,6 +35,6 @@ cacheSolve <- function(x, ...)
   }
   inverse <- solve(x$get())
   x$setInverse(inverse)
-  inverse
   ## Return a matrix that is the inverse of 'x'
+  inverse
 }
